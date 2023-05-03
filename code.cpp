19d96e7b10681c24ed963cf69c0fcd185421d91c
Sahil Kansal <kansalsahil51@gmail.com>
2023-05-03 11:28:49 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+float percent(float pr,float total);
+
+void skip(float pr, float total){
+    int counter=0;
+    while(percent(pr,total)>=74.01){
+        counter++;
+        total++;
+    }
+    if(!(percent(pr,total)>=74.01)){
+        counter--;
+    }
+    if(counter==0){
+        cout<<"You are on the verge, just go all the classes!";
+    }
+    else{
+        cout<<"Class can be skipped: "<<counter;
+    }
+}
+
+void attend(float pr, float total){
+    int temp=pr;
+    while(percent(temp, total)<=74.01){
+        temp++;
+        total++;
+    }
+    cout<<"Class needed to be attended: "<<temp-pr<<endl;
+}
+bool can_skip(float pr,float total){
+    float per=(pr*100)/total;
+    if(per>=74.01){
+        return true;
+    }
+    return false;
+}
+
+float percent(float pr,float total){
+    return (pr*100)/total;
+    
+}
+int main(){
+    float total;
+    float pr;
+    cout<<"Enter the total no of classes happened: ";
+    cin>>total;
+    cout<<"Enter the total no of present days: ";
+    cin>>pr;
+    cout<<"Current Attendance percentage is: "<<ceil(percent(pr, total))<<endl;
+    if(can_skip(pr, total)){
+        skip(pr, total);
+    }
+    else{
+        attend(pr, total);
+    }
+}
