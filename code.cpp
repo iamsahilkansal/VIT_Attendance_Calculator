406d065088dd0888a613928ad8ee2cd7dbd76ed0
Sahil Kansal <kansalsahil51@gmail.com>
2023-05-04 23:31:05 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,59 +1,69 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-float percent(float pr,float total);
+float percent(float pr, float total);
 
-void skip(float pr, float total){
-    int counter=0;
-    while(percent(pr,total)>=74.01){
+void skip(float pr, float total)
+{
+    int counter = 0;
+    while (percent(pr, total) >= 74.01)
+    {
         counter++;
         total++;
     }
-    if(!(percent(pr,total)>=74.01)){
+    if (!(percent(pr, total) >= 74.01))
+    {
         counter--;
     }
-    if(counter==0){
-        cout<<"You are on the verge, just go all the classes!";
+    if (counter == 0)
+    {
+        cout << "You are on the verge, just go all the classes!";
     }
-    else{
-        cout<<"Class can be skipped: "<<counter<<endl;
-        cout<<"Attendance percentage will be: "<<ceil(percent(pr, total-1))<<"%"<<endl;
+    else
+    {
+        cout << "Class can be skipped: " << counter << endl;
     }
 }
 
-void attend(float pr, float total){
-    int temp=pr;
-    while(percent(temp, total)<=74.01){
+void attend(float pr, float total)
+{
+    int temp = pr;
+    while (percent(temp, total) <= 74.01)
+    {
         temp++;
         total++;
     }
-    cout<<"Class needed to be attended: "<<temp-pr<<endl;
-    cout<<"Attendance percentage will be: "<<ceil(percent(temp, total))<<"%"<<endl;
+    cout << "Class needed to be attended: " << temp - pr << endl;
 }
-bool can_skip(float pr,float total){
-    float per=(pr*100)/total;
-    if(per>=74.01){
+bool can_skip(float pr, float total)
+{
+    float per = (pr * 100) / total;
+    if (per >= 74.01)
+    {
         return true;
     }
     return false;
 }
 
-float percent(float pr,float total){
-    return (pr*100)/total;
-    
+float percent(float pr, float total)
+{
+    return (pr * 100) / total;
 }
-int main(){
+int main()
+{
     float total;
     float pr;
-    cout<<"Enter the total no of classes happened: ";
-    cin>>total;
-    cout<<"Enter the total no of present days: ";
-    cin>>pr;
-    cout<<"Current Attendance percentage is: "<<ceil(percent(pr, total))<<"%"<<endl;
-    if(can_skip(pr, total)){
+    cout << "Enter the total no of classes happened: ";
+    cin >> total;
+    cout << "Enter the total no of present days: ";
+    cin >> pr;
+    cout << "Current Attendance percentage is: " << ceil(percent(pr, total)) << "%" << endl;
+    if (can_skip(pr, total))
+    {
         skip(pr, total);
     }
-    else{
+    else
+    {
         attend(pr, total);
     }
 }
